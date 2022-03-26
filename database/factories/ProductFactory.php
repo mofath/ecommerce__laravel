36caa40ebdc5e2b04d9a82b3435738c938e2e32c
mofath <mohamed.elgaedi@gmail.com>
2022-03-26 13:50:52 +0200

    Feat(models): Added Product model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $name = $this->faker->unique()->name,
+            'slug' => Str::slug($name, '-'),
+            'description' => $this->faker->sentence,
+            'price' => rand(200, 2000)
+        ];
+    }
+}
