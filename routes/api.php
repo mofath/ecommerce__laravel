85a53520e284fc421db83ddbe23c9767324146f5
mofath <mohamed.elgaedi@gmail.com>
2022-03-26 13:52:30 +0200

    Feat(api): Added produts index endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Categories\CategoryController;
+use App\Http\Controllers\Products\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
