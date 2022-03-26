1c4b4323c20c27a274de9f8851b9791182c7a52a
mofath <mohamed.elgaedi@gmail.com>
2022-03-26 02:42:52 +0200

    Feat(test): Test category moel

---
+++ tests/Unit/Models/Categories/CaregoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Models\Categories;
+
+use App\Models\Category;
+use Tests\TestCase;
+
+class CaregoryTest extends TestCase
+{
+
+    public function test_it_has_children()
+    {
+        $category = Category::factory()->create();
+        $category->children()->save(
+            Category::factory()->create()
+        );
+
+        $this->assertInstanceOf(Category::class, $category->children->first());
+    }
+
+    public function test_it_can_fetch_only_parents()
+    {
+        $category = Category::factory()->create();
+        $category->children()->save(
+            Category::factory()->create()
+        );
+
+        $this->assertEquals(1, Category::parents()->count());
+    }
+
+    public function test_it_is_orderable_by_a_number_order()
+    {
+        $category1 = Category::factory()->create([
+            'order' => 2
+        ]);
+        $category2 = Category::factory()->create([
+            'order' => 1
+        ]);
+
+        $this->assertEquals($category2->name, Category::ordered()->first()->name);
+    }
+}
